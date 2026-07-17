9245fb72e77dbad6c8169274d8374af9b2354178
3160598707-cloud <3160598707@qq.com>
2026-07-18 01:17:25 +0800

    feat: github-pages static export for China access

--- public/api-proxy.js
+++ public/api-proxy.js
@@ -2,17 +2,17 @@
 (function() {
   var origFetch = window.fetch;
   window.fetch = function(url, options) {
-    if (typeof url === 'string' && url.indexOf('/api/') === 0) {
-      // 去掉可能的查询参数
-      var q = url.indexOf('?');
-      var path = q >= 0 ? url.substring(0, q) : url;
-      // 已有 .json 后缀就不加
-      if (!path.match(/\.json$/)) {
-        url = path + '.json' + (q >= 0 ? url.substring(q) : '');
-      }
-      // POST 改 GET
-      if (options && options.method === 'POST') {
-        options = Object.assign({}, options, { method: 'GET' });
+    if (typeof url === 'string') {
+      var idx = url.indexOf('/api/');
+      if (idx >= 0) {
+        var q = url.indexOf('?');
+        var path = q >= 0 ? url.substring(0, q) : url;
+        if (!path.match(/\.json$/)) {
+          url = path + '.json' + (q >= 0 ? url.substring(q) : '');
+        }
+        if (options && options.method === 'POST') {
+          options = Object.assign({}, options, { method: 'GET' });
+        }
       }
     }
     return origFetch.call(window, url, options);
